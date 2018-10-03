22d23d66bc95cca558216d7ad04bf919c311f0ed
Alexito101 <cuatroalejandro@gmail.com>
2018-10-03 01:42:40 -0400

    first commit

---
+++ TypeCard2/Assets/codigo/levelChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class levelChange : MonoBehaviour {
+
+	public Animator animator;
+	private int levelToLoad;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Invoke("fadeToLevel",0.1f);
+	}
+
+	public void fadeToLevel(int LevelIndex){
+		levelToLoad = LevelIndex;
+		animator.SetTrigger("fadeOut");
+		
+	}
+
+	public void OnFadeComplete(){
+		SceneManager.LoadScene(1);
+	}
+}
---
+++ TypeCard2/Assets/codigo/link.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class link : MonoBehaviour {
+void Start () {
+		
+	}
+
+    public void type()
+    {
+        Application.OpenURL("https://www.typeiqs.com/");
+    }
+
+    public void medium()
+    {
+        Application.OpenURL("https://medium.com/typeiqs");
+    }
+
+	 public void email()
+    {
+        Application.OpenURL("https://www.google.com.mx/?hl=es-419");
+    }
+
+	 public void linked()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/alexis-fraudita-macia-169b0515a/");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
